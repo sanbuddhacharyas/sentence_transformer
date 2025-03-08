082a3c400d9836cd895a10cefcec2bf4ff9c0979
sanbuddhacharyas <sangambuddhacharya@gmail.com>
2025-03-07 18:28:51 -0800

    Add dataloader to download dataset from hugging face

---
+++ utils/dataloader.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+
+
+def load_huggingFace_dataset(dataset_name:str, dataset_type='pair'):
+    train_dataset = load_dataset(dataset_name, dataset_type, split='train')
+    test_dataset  = load_dataset(dataset_name, dataset_type, split='test')
+
+    return train_dataset, test_dataset
+
+
+
+
+
+if __name__ == '__main__':
+    dataset_name = 'sentence-transformers/all-nli'
+    train_dataset, test_dataset = load_huggingFace_dataset(dataset_name)
+    print(train_dataset[0])
